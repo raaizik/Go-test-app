508a0b9458ed295fc53dbb09083191f701ed424b
raaizik <132667934+raaizik@users.noreply.github.com>
2023-05-28 15:44:36 +0300

    Updates

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,4 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
-*/
+// Package cmd /*
 package cmd
 
 import (
@@ -14,13 +11,9 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "Go-test-app",
-	Short: "A brief description of your application ",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A chess CLI app",
+	Long: `
+This app is a tool that utilizes golang's chess package`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
