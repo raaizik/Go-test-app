e2c0d3dbcd8fc9046185854640aa39a15d52ea2d
raaizik <132667934+raaizik@users.noreply.github.com>
2023-05-28 12:51:09 +0300

    findOpening works OK
    with databases (concatenates .pgn files and displays the list of their respective openings)

--- cmd/findOpening.go
+++ cmd/findOpening.go
@@ -9,7 +9,9 @@ import (
 	"github.com/notnil/chess"
 	"github.com/notnil/chess/opening"
 	"github.com/spf13/cobra"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -17,44 +19,124 @@ import (
 var findOpeningCmd = &cobra.Command{
 	Use:   "findOpening",
 	Short: "Returns the opening used in the received PGN",
-	Long:  `Opens a PGN file name (w/o .pgn extension), saved to PGNs folder in project's root dir, of a game and returns the opening used according to the ECO.`,
+	Long:  `Opens a PGN file or a folder that is database of PGN files, saved to PGNs folder in project's root dir, and returns the opening used according to the ECO.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) == 0 {
-			fmt.Println("No PGN file name received as argument.")
+			fmt.Println("No file name received as argument.")
 			return
 		}
 
-		pgnFilename := args[0]
+		pgnDBFilename := args[0]
 
 		// remove .pgn file extension if any
-		if strings.Contains(pgnFilename, ".pgn") {
-			pgnFilename = strings.ReplaceAll(pgnFilename, ".pgn", "")
+		if strings.Contains(pgnDBFilename, ".pgn") {
+			pgnDBFilename = strings.ReplaceAll(pgnDBFilename, ".pgn", "")
 		}
-		f, err := os.Open("/Users/il015040/workspace/Go-test-app/PGNs/" + pgnFilename + ".pgn")
-		if err != nil {
-			panic(err)
+		pwd, _ := os.Getwd()
+		dbPath := filepath.Join(pwd, "/PGNs/"+pgnDBFilename)
+		//path, err := os.Open(filepath.Join(pwd, "/PGNs/"+pgnDBFilename))
+
+		items, _ := ioutil.ReadDir(dbPath)
+		for _, item := range items {
+			if item.IsDir() {
+				fmt.Println("Bad structure. DB folder must contain only .pgn files")
+				//subitems, _ := ioutil.ReadDir(item.Name())
+				//for _, subitem := range subitems {
+				//	if !subitem.IsDir() {
+				//		// handle file there
+				//		fmt.Println(item.Name() + "/" + subitem.Name())
+				//	}
+				//}
+			} else {
+				// handle file there
+				//fmt.Println(item.Name())
+				f, err := os.Open(filepath.Join(dbPath, item.Name()))
+				//fmt.Printf("File Name: %s\n", info.Name())
+				//return nil
+				if err != nil {
+					panic(err)
+				}
+
+				// data leaks when calling defer inside the for loop (all files will remain open until function
+				// returns)
+				//defer f.Close()
+
+				scanner := chess.NewScanner(f)
+
+				game := chess.NewGame()
+				//games := make([]*chess.Game, 0, 100)
+
+				// scans first PGN in the file. If the file contains more than one PGN, it won't be included
+				for scanner.Scan() {
+					game = scanner.Next()
+					//games = append(games, game)
+				}
+
+				//for _, game := range games {
+				reader := strings.NewReader(game.String())
+				pgn, err := chess.PGN(reader)
+				if err != nil {
+					// handle error
+					fmt.Printf("Something went wrong, got error from %v", err)
+				}
+
+				g := chess.NewGame(pgn)
+
+				// print opening name
+				book := opening.NewBookECO()
+				o := book.Find(g.Moves())
+				fmt.Println(item.Name(), " - ", o.Title())
+				//}
+				f.Close()
+			}
 		}
-		defer f.Close()
 
-		scanner := chess.NewScanner(f)
-		game := chess.NewGame()
-		for scanner.Scan() {
-			game = scanner.Next()
-		}
+		//filepath.Walk(dbPath, func(path string, info os.FileInfo, err error) error {
+		//	if err != nil {
+		//		log.Fatalf(err.Error())
+		//	}
+		//	f, err := os.Open(filepath.Join(dbPath, info.Name()))
+		//	//fmt.Printf("File Name: %s\n", info.Name())
+		//	//return nil
+		//	if err != nil {
+		//		panic(err)
+		//	}
+		//	defer f.Close()
+		//
+		//	scanner := chess.NewScanner(f)
+		//	// todo: this grabs the first PGN from the db file.
+		//	// can be more complex to have used choose which game from the db file to display
+		//	// OR print opening for each one of the PGNs
+		//	game := chess.NewGame()
+		//	games := make([]*chess.Game, 0, 100)
+		//
+		//	for scanner.Scan() {
+		//		game = scanner.Next()
+		//		games = append(games, game)
+		//	}
+		//	// todo: still grabs only the first PGN in the db
+		//	fmt.Println(len(games))
+		//	for _, game := range games {
+		//		reader := strings.NewReader(game.String())
+		//		pgn, err := chess.PGN(reader)
+		//		if err != nil {
+		//			// handle error
+		//			fmt.Printf("Something went wrong, got error from %v", err)
+		//			return err
+		//		}
+		//
+		//		g := chess.NewGame(pgn)
+		//
+		//		// print opening name
+		//		book := opening.NewBookECO()
+		//		o := book.Find(g.Moves())
+		//		fmt.Println(o.Title())
+		//	}
+		//	return nil
+		//})
 
-		pgn, err := chess.PGN(strings.NewReader(game.String()))
-		if err != nil {
-			// handle error
-			fmt.Printf("Something went wrong, got error from %v", err)
-			return
-		}
+		//f, err := os.Open("/Users/il015040/workspace/Go-test-app/PGNs/" + pgnDBFilename + "/*.pgn")
 
-		g := chess.NewGame(pgn)
-
-		// print opening name
-		book := opening.NewBookECO()
-		o := book.Find(g.Moves())
-		fmt.Println(o.Title())
 	},
 }
 
